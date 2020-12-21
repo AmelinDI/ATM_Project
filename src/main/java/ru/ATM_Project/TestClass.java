27568397f8da2c6a6fc07db00474255794c2512e
AmelinDI <damelin@rambler.ru>
2020-12-22 01:32:45 +0300

    Sonar

--- src/main/java/ru/ATM_Project/TestClass.java
+++ src/main/java/ru/ATM_Project/TestClass.java
@@ -2,6 +2,8 @@ package ru.ATM_Project;
 
 public class TestClass {
     void printString(String input){
-        System.out.println(input);
+        System.out.print(input);
+        System.out.println();
+        System.out.println();
     }
 }
