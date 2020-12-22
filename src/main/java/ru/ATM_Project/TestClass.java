29e9255e883905f592e27417c861b1eaa2dd7768
AmelinDI <damelin@rambler.ru>
2020-12-22 23:36:48 +0300

    Sonar

--- src/main/java/ru/ATM_Project/TestClass.java
+++
@@ -1,9 +0,0 @@
-package ru.ATM_Project;
-
-public class TestClass {
-    void printString(String input){
-        System.out.print(input);
-        System.out.println();
-        System.out.println();
-    }
-}
